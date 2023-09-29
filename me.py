e66e6cf96e6cfbbd2f5fbca01776d11c7b4f3529
matebadzagua <146456473+matebadzagua@users.noreply.github.com>
2023-09-29 07:43:01 -0700

    Add files via upload

---
+++ me.py
@@ -0,0 +1,14 @@
+my_age=14
+ded_age=37
+mom_age=39
+mybrother_age=11
+myseqondbrother_age=10
+mysister_age=18
+myseqondsister_age=8
+print(my_age+5)
+print(ded_age+5)
+print(mom_age+5)
+print(mybrother_age+5)
+print(myseqondbrother_age+5)
+print(mysister_age+5)
+print(myseqondsister_age+5)
